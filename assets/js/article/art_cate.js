cce3f542cea13c8c298ac2ddf23f73fb7dd7ec20
偏执 <1924618143@qq.com>
2022-06-12 19:28:21 +0800

    完善了基本功能

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,4 +1,5 @@
 $(function () {
+  const form = layui.form;
   // // 获取 表格数据
   const initartCateList = () => {
     $.ajax({
@@ -14,7 +15,7 @@ $(function () {
   const layer = layui.layer;
   let indexAdd = null;
   $('#btnAddCate').click(() => {
-    layer.open({
+    indexAdd = layer.open({
       type: 1,
       area: ['500px', '250px'],
       title: '添加文章分类',
@@ -37,5 +38,54 @@ $(function () {
     });
   });
 
+  let indexEdit = null;
+  $('tbody').on('click', '.btn_edit', function () {
+    const id = $(this).attr('data-id');
+    indexEdit = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '修改文章分类',
+      content: $('#dialog-edit').html(),
+    });
+    $.ajax({
+      type: 'GET',
+      url: '/my/article/cates/' + id,
+      success: (res) => {
+        if (res.status !== 0) return layer.msg('获取分类数据失败！');
+        form.val('form-edit', res.data);
+      },
+    });
+  });
+  // 修改文章分类
+  $('body').on('submit', '#form-edit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/my/article/updatecate',
+      data: $(this).serialize(),
+      success: (res) => {
+        if (res.status !== 0) return layer.msg('更新分类数据失败！');
+        layer.msg('更新分类数据成功！');
+        initartCateList();
+        layer.close(indexEdit);
+      },
+    });
+  });
+  // 删除文章分类
+  $('tbody').on('click', '.btn-delete', function () {
+    const id = $(this).attr('data-id');
+    layer.confirm('确定删除吗？', { icon: 3, title: '提示' }, function (index) {
+      $.ajax({
+        type: 'GET',
+        url: '/my/article/deletecate/' + id,
+        success: (res) => {
+          if (res.status !== 0) return layer.msg('删除分类失败！');
+          layer.msg('删除分类成功！');
+          initartCateList();
+          layer.close(index);
+        },
+      });
+    });
+  });
   initartCateList();
 });
